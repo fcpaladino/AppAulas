34a9e7d05a1ee4ee40ba3bd2f66d38a1d0cbf8f4
Filipe Paladino <filipe.paladino@webee.com.br>
2015-11-18 02:02:30 -0200

    Mostra imagem do mapa.

--- app/src/main/java/filipepaladino/appaula/view/view/Base.java
+++ app/src/main/java/filipepaladino/appaula/view/view/Base.java
@@ -1,6 +1,8 @@
 package filipepaladino.appaula.view.view;
 
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,12 +17,15 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -36,12 +41,6 @@ public class Base extends AppCompatActivity {
         setContentView(R.layout.base);
 
         imvMapa = (ImageView)findViewById(R.id.imvMapa);
-
-        String imageUrl = "http://maps.googleapis.com/maps/api/staticmap?center=Rua+sergipe,+londrina&zoom=14&scale=1&size=600x300&maptype=roadmap&key=AIzaSyD7O7QDbeCBDCQwlrldLM5t2VQeqHH1Kjs&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0x0000ff%7Clabel:A%7CRua+sergipe,+londrina";
-
-
-
-
     }
 
     @Override
@@ -105,7 +104,15 @@ public class Base extends AppCompatActivity {
                                 builder.append(formatedAddress + "\n");
                             }
 
-                            ((TextView)findViewById(R.id.txvResultado)).setText(builder);
+                            JSONObject location = result.getJSONObject(0).getJSONObject("geometry").getJSONObject("location");
+
+                            Double geoLat = location.getDouble("lat");
+                            Double geoLng = location.getDouble("lng");
+
+                            getImageMap(geoLat, geoLng);
+
+                            ((TextView) findViewById(R.id.txvLocation)).setText(geoLat+", "+geoLng);
+                            ((TextView) findViewById(R.id.txvResultado)).setText(builder);
                         } catch (Exception ex) {
                             Toast.makeText(
                                     Base.this,
@@ -126,6 +133,34 @@ public class Base extends AppCompatActivity {
                     }
                 }
         );
+
+        RequestQueue fila = Volley.newRequestQueue(this);
+        fila.add(request);
+    }
+
+
+
+    public void getImageMap(Double lat, Double lng){
+        //String url = "http://maps.google.com/maps/api/staticmap?center=" +lat + "," + lng + "&zoom=15&size=400x400&sensor=false";
+        //String url = "http://maps.google.com/maps/api/staticmap?center=-23.3098282,-51.16150349999999&zoom=15&size=400x400&sensor=false";
+        //String url = "http://maps.googleapis.com/maps/api/staticmap?center=-23.3098282,-51.16150349999999&zoom=14&scale=1&size=600x300&maptype=roadmap&key=AIzaSyD7O7QDbeCBDCQwlrldLM5t2VQeqHH1Kjs&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0x0000ff%7Clabel:A%7C-23.3098282,-51.16150349999999";
+
+        String url = "http://maps.googleapis.com/maps/api/staticmap?autoscale=1&size=400x400&maptype=roadmap&key=AIzaSyD7O7QDbeCBDCQwlrldLM5t2VQeqHH1Kjs&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0x0000ff%7Clabel:A%7C"+lat+","+lng;
+
+
+
+        ImageRequest request = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        imvMapa.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Base.this, "Erro imagem", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         RequestQueue fila = Volley.newRequestQueue(this);
         fila.add(request);
