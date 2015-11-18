a2e56eb1800b4e2c0ea1650c32cd8782be7794c7
Filipe Paladino <filipe.paladino@webee.com.br>
2015-11-18 00:29:29 -0200

    Geocode do google maps.

--- app/src/main/java/filipepaladino/appaula/view/view/Base.java
+++ app/src/main/java/filipepaladino/appaula/view/view/Base.java
@@ -1,9 +1,27 @@
 package filipepaladino.appaula.view.view;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import filipepaladino.appaula.R;
 
@@ -35,5 +53,70 @@ public class Base extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void executarPesquisa(View view) {
+        final ProgressDialog progresso = new ProgressDialog(this);
+        progresso.setTitle("Por favor espere!");
+        progresso.setCancelable(false);
+
+        progresso.show();
+
+        String endereco = ((EditText)findViewById(R.id.edxPesquisa)).getText().toString();
+
+        String query = null;
+        try {
+            query = URLEncoder.encode(endereco, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query;
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                "",
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        progresso.hide();
+
+                        try {
+                            StringBuilder builder = new StringBuilder();
+
+                            JSONArray result = response.getJSONArray("results");
+                            for (int i = 0; i < result.length(); i++)
+                            {
+                                JSONObject endereco = result.getJSONObject(i);
+                                String formatedAddress =
+                                        endereco.getString("formatted_address");
+                                builder.append(formatedAddress + "\n");
+                            }
+
+                            ((TextView)findViewById(R.id.txvResultado)).setText(builder);
+                        } catch (Exception ex) {
+                            Toast.makeText(
+                                    Base.this,
+                                    ex.toString(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progresso.hide();
+
+                        Toast.makeText(
+                                Base.this,
+                                error.toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        RequestQueue fila = Volley.newRequestQueue(this);
+        fila.add(request);
     }
 }
